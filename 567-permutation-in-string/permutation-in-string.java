fd60e7cf0fd1d0a41abbd75a33112f5d8dc7acb8
Dev Kumar Jadaun <devthakur6920@gmail.com>
2026-09-09 11:15:02 +0530

    Time: 6 ms (87.40%) | Memory: 44.1 MB (48.16%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.java
@@ -0,0 +1,33 @@
+// s1.lenght()ht2import java.util.Arrays;
+
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            freq1[s1.charAt(i) - 'a']++;
+        }
+
+        int k = s1.length();
+        for (int i = 0; i < k; i++) {
+            freq2[s2.charAt(i) - 'a']++;
+        }
+
+        if (Arrays.equals(freq1, freq2)) {
+            return true;
+        }
+        for (int i = k; i < s2.length(); i++) {
+            freq2[s2.charAt(i) - 'a']++;
+            freq2[s2.charAt(i - k) - 'a']--;
+            if (Arrays.equals(freq1, freq2)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
